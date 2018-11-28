48c8ab4383064dbaeed4148d7986bcd29de3232e
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2018-11-28 12:19:16 +0100

    obstacle class

---
+++ Vertical Slice/Assets/Scripts/Movement/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void Update () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        rb2d.AddForce(move * speed);
+
+    }
+}
