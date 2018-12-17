305178bf97b12b00aa8f26dc72079150589d62b7
NoahHoi365 <24922@ma-web.nl>
2018-12-17 09:18:33 +0100

    katapult lijnen (bijna af)

--- Vertical Slice/Assets/Scripts/Objects/Katapult.cs
+++ Vertical Slice/Assets/Scripts/Objects/Katapult.cs
@@ -10,26 +10,48 @@ public class Katapult : MonoBehaviour {
 	private Transform startPosition;
 	[SerializeField]
 	private List<Bird> amunition = new List<Bird>();
+    [SerializeField]
+    private List<GameObject> dragLines;
 
 	void Start()
 	{
         StartCoroutine(loadCatapult(0));
-	}
+    }
 
-	void Update()
-	{
-		if(amunition.Count > 0){
-			if(!isLoaded){
-				StartCoroutine(loadCatapult(3));
-			}
-			amunition[0].StartCoroutine("Drag");
-			if(amunition[0].released){
-				amunition[0].StartCoroutine(amunition[0].Shoot(startPosition));
-				amunition.RemoveAt(0);
-				isLoaded = false;
-			}
-		}
-	}
+    void Update()
+    {
+        if (amunition.Count > 0)
+        {
+            if (!isLoaded)
+            {
+                StartCoroutine(loadCatapult(3));
+            }
+            amunition[0].StartCoroutine("Drag");
+            if (amunition[0].released)
+            {
+                amunition[0].StartCoroutine(amunition[0].Shoot(startPosition));
+                amunition.RemoveAt(0);
+                isLoaded = false;
+            }
+            DragLines();
+        }
+        
+    }
+
+    void DragLines()
+    {
+        for (int i = 0; i < dragLines.Count; i++)
+        {
+            Debug.Log("oof");
+            Vector2 distance = amunition[0].transform.position - dragLines[i].transform.position;
+            dragLines[i].transform.localScale = new Vector2(distance.magnitude/2, 1);
+            dragLines[1].transform.localScale = new Vector2(distance.magnitude/1.13f, 1);
+            float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+            angle += 180;
+            Debug.Log(angle);
+            dragLines[i].transform.localEulerAngles = new Vector3(0, 0, angle); 
+        }
+    }
 
 	private IEnumerator loadCatapult(int time){
 		isLoaded = true;
